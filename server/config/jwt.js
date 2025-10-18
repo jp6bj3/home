c9e85a2a9c89592b4df1ba5ddc736a2a459f321b
una <una@unadeMacBook-Air.local>
2025-10-18 19:23:26 +0800

    改後台api

--- server/config/jwt.js
+++ server/config/jwt.js
@@ -6,7 +6,7 @@ export const jwtConfig = {
   secret: process.env.JWT_SECRET || '@JWT_SECRET',
   refreshSecret: process.env.JWT_REFRESH_SECRET || '@JWT_REFRESH_SECRET',
   accessExpiry: process.env.JWT_ACCESS_EXPIRY || '@JWT_ACCESS_EXPIRY',
-  refreshExpiry: process.env.JWT_REFRESH_EXPIRY || '@WTS_REFRESH_EXPIRYS',
+  refreshExpiry: process.env.JWT_REFRESH_EXPIRY || '@JWT_REFRESH_EXPIRYS',
   cookieOptions: {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
